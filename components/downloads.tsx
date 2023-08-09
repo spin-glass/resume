9784adfc2b17bd9c078c917795a9eee9f3524305
Toshihiro Yamaguchi <toshihiro.engineer@gmail.com>
2023-08-09 19:16:07 +0900

    Modify Download button

--- components/downloads.tsx
+++ components/downloads.tsx
@@ -1,12 +1,14 @@
 function DownloadButton() {
   return (
+    <div className="mt-20">
     <a
       href="/resume-ja.pdf"
       download
       className="bg-gray-300 hover:bg-gray-400 text-black font-bold rounded px-6 py-2 m-1 transition duration-150 ease-in-out"
     >
-      pdfダウンロード
+      pdf
     </a>
+    </div>
   );
 }
 
