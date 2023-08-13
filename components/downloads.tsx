dc62ac8e1c9c95256b10f6011e638de504668d7d
Toshihiro Yamaguchi <toshihiro.engineer@gmail.com>
2023-08-13 16:48:00 +0900

    Update resume format

--- components/downloads.tsx
+++ components/downloads.tsx
@@ -1,15 +1,15 @@
 function DownloadButton() {
   return (
     <div className="flex justify-end mt-20">
-    <div>
-    <a
-      href="/resume-ja.pdf"
-      download
-      className="bg-gray-300 hover:bg-gray-400 text-black font-bold rounded px-6 py-2 m-1 transition duration-150 ease-in-out"
-    >
-      pdf
-    </a>
-    </div>
+      <div>
+        <a
+          href="/assets/resume-ja.pdf"
+          download
+          className="bg-gray-300 hover:bg-gray-400 text-black font-bold rounded px-6 py-2 m-1 transition duration-150 ease-in-out"
+        >
+          pdf
+        </a>
+      </div>
     </div>
   );
 }
