267a30f0be67ac3bec2acb738422b62c916c018a
Toshihiro Yamaguchi <toshihiro.engineer@gmail.com>
2024-08-16 18:20:06 +0900

    Remove index

--- middleware.ts
+++ middleware.ts
@@ -4,21 +4,6 @@ export const config = {
   matcher: ["/", "/index"],
 };
 
-export function middleware(req: NextRequest) {
-  // const basicAuth = req.headers.get("authorization");
-  // const url = req.nextUrl
-
-  // if (basicAuth) {
-  //   const authValue = basicAuth.split(" ")[1];
-  //   // const [user, pwd] = Buffer.from(authValue, 'base64').toString('utf-8').split(":");
-  //   const [user, pwd] = atob(authValue).split(':')
-  //   if (user === process.env.username && pwd === process.env.password) {
-  //     return NextResponse.next()
-  //   }
-  // }
-  // url.pathname = '/api/auth'
-
-  // return NextResponse.rewrite(url)
-}
+export function middleware(req: NextRequest) {}
 
 export default middleware;
